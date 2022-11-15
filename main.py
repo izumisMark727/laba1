49bb5f8d9eede4214c41d5f1c3f443b3449f5393
izumisMark727 <nacuomaki@gmail.com>
2022-11-16 01:02:47 +0300

    roots of the square equasion formula

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+'''sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
 sides = sorted(sides, reverse=True)
 smax = 0
 for i in range(len(sides)):
@@ -12,4 +12,12 @@ for i in range(len(sides)):
                 s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
                 if s > smax:
                     smax = s
-print("Максимальная площадь треугольника", smax)
+print("Максимальная площадь треугольника", smax)'''
+ #homework
+a=int(input())
+b=int(input())
+c=int(input())
+if b**2-4*a*c>=0: #проверка неотрицательности дискриминанта
+    print((b*(-1)+(b**2-4*a*c)**0.5)/2*a,(b*(-1)-(b**2-4*a*c)**0.5)/2*a) #2 формулы для корней
+else:
+    print("No real roots")
